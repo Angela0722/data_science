6127f3f670917e019d23feddb18b8a31819f8c93
Angela0722 <162700369+Angela0722@users.noreply.github.com>
2024-03-08 12:39:46 +0100

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,9 @@
+## input function
+people= float(input("how many people are in the class:"))
+print(people)
+
+total_money= 1000
+quantity= 3
+price= 450
+expected_output= "i have $ {}, so i can buy {} footballs, for $ {}"
+print(expected_output.format(total_money,quantity,price))
---
+++ Day 1.py
@@ -0,0 +1,46 @@
+## variables
+## variables naming cannot start with digits
+## variables naming cannot have any special characters
+## variable naming cannot contain a space
+
+
+num=32 ## integers
+num1=12.0 ## float
+name="Early code" ## string
+name2=True ## Booleans
+
+
+print(num)
+print(num1)
+print(name)
+print(name2)
+
+
+## string formatting
+## Backward slash
+word= "we\'re are brothers from the town"
+print(word)
+
+
+num6=34.8 ## float
+print(num6)
+
+
+school= "i am in class already"
+print(school)
+
+text8=45.6
+print(text8)
+
+## New line string
+word2="python is fun\npython  is easy\npython is great"
+print(word2)
+
+## multiline string
+word4= '''python is fun
+python is easy
+python is interesting'''
+print(word4)
+
+## string concatenation
+print("Hello" + "World")
