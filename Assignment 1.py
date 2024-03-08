2aa766546d4ef445e35ca781419f40053ffc53e5
Angela0722 <162700369+Angela0722@users.noreply.github.com>
2024-03-08 19:49:33 +0100

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,16 @@
+## string
+
+someone= "MY NAME IS JAMES"
+print(someone)
+
+state= 'from a well-known village with good culture in kogi state'
+print(state)
+
+apparels= "shoe\nbag\nbelt\ndress"
+print(apparels)
+
+POEM= '''THOUGHT SHE WAS MY FRIEND
+THE BEST ONE I HAVE HAD
+INSTEAD I FOUND OUT THE TRUTH
+AND WHAT I LEARNED WAS SAD'''
+print(POEM)
